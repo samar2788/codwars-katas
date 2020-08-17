5976306dbbafec73073aa2b62e732ef98c99f85b
Samar Bhargava <samarbhargava@yahoo.co.in>
2020-08-17 19:37:04 +0530

    testing the new ssh key

--- try.py
+++ try.py
@@ -1,10 +1,3 @@
-a = 'aBc_e9g*i-k$m'
-fl=[]
-if len(a) <2 or len(a)>100:
-    print("invalid string")
-else:
-    al=len(a)
-    for i in range(al):
-        if i%2!=0:
-            fl.append(a[i])
-    print(fl)
+def sum_two_smallest_numbers(numbers):
+    #your code here
+test
