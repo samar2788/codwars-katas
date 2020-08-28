a6d9d9e7eeff93456c7c6a2c4a55655e1d4d9c9e
Samar Bhargava <samarbhargava@yahoo.co.in>
2020-08-28 23:30:31 +0530

    a few more katas

--- weirdaddition.py
+++ weirdaddition.py
@@ -1,18 +1,18 @@
-def add(num1,num2):
-    sl=[]
-    fs=''
+def add(num1, num2):
+    sl = []
+    fs = ''
     if len(str(num1)) == len(str(num2)):
-        ln1=[int(i) for i in str(num1)]
-        ln2 =[int(i) for i in str(num2)]
+        ln1 = [int(i) for i in str(num1)]
+        ln2 = [int(i) for i in str(num2)]
         for i in range(len(ln1)):
-            sum=ln1[i]+ln2[i]
+            sum = ln1[i]+ln2[i]
             sl.append(sum)
         for i in sl:
-            fs+=str(i)
-        return fs
+            fs += str(i)
+        return int(fs)
     elif len(str(num1)) < len(str(num2)):
         zta = len(str(num2)) - len(str(num1))
-        nnum1=zta*'0' + str(num1)
+        nnum1 = zta*'0' + str(num1)
         ln1 = [int(i) for i in (nnum1)]
         ln2 = [int(i) for i in str(num2)]
         for i in range(len(ln1)):
@@ -20,7 +20,7 @@ def add(num1,num2):
             sl.append(sum)
         for i in sl:
             fs += str(i)
-        return fs
+        return int(fs)
     elif len(str(num1)) > len(str(num2)):
         zta = len(str(num1)) - len(str(num2))
         nnum2 = zta*'0' + str(num2)
@@ -31,6 +31,4 @@ def add(num1,num2):
             sl.append(sum)
         for i in sl:
             fs += str(i)
-        return fs
-
-print(add(122,81))
+        return int(fs)
