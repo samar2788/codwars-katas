175dec266c950fde84a106fc3954c51c585449e6
Samar Bhargava <samarbhargava@yahoo.co.in>
2021-05-29 18:00:38 +0530

    Ease the stockbroker

--- ease_stock_broker.py
+++ ease_stock_broker.py
@@ -1,20 +1,19 @@
 def balance_statement(lst):
-    buy=[]
-    sell=[]
-    sumbuy=0
-    sumsell=0
-    errl=[]
-    err=0
-    if lst=='':
+    buy = []
+    sell = []
+    sumbuy = 0
+    sumsell = 0
+    errl = []
+    err = 0
+    if lst == '':
         return "Buy: 0 Sell: 0"
-    elif lst!='':
+    elif lst != '':
         ls = lst.split(', ')
         for i in ls:
-            tl=i.split(' ')
-            tll=len(tl)
-            print(tll)
-            if tll!=4 or '.' in tl[1] or '.' not in tl[2] or tl[0]=='' or tl[3]=='' or tl[3] not in 'BS':
-                err+=1
+            tl = i.split(' ')
+            tll = len(tl)
+            if tll != 4 or '.' in tl[1] or '.' not in tl[2] or tl[0] == '' or tl[3] == '' or tl[3] not in 'BS':
+                err += 1
                 errl.append(i)
             else:
                 if tl[3] == 'B':
@@ -23,14 +22,13 @@ def balance_statement(lst):
                 elif tl[3] == 'S':
                     tt = float(tl[1])*float(tl[2])
                     sell.append(tt)
-    sumbuy=round(sum(buy))
-    sumsell=round(sum(sell))
-    ers=' ;'.join(errl)
-    if err>0:
+    sumbuy = round(sum(buy))
+    sumsell = round(sum(sell))
+    ers = ' ;'.join(errl)
+    if err > 0:
         return f"Buy: {sumbuy} Sell: {sumsell}; Badly formed {err}: {ers} ;"
     else:
         return f"Buy: {sumbuy} Sell: {sumsell}"
-
 
 
 print(balance_statement(
@@ -39,6 +37,3 @@ print(balance_statement(
 print(balance_statement(
     "GOOG 90 160.45 B, JPMC 67 12.8 S, MYSPACE 24.0 210 B, CITI 50 450 B, CSCO 100 55.5 S"))
 # "Buy: 29499 Sell: 0")
-
-
-
